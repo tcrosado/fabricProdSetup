5eab34e723f22fcbf29aae6b8d33459c52746e8e
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2018-09-19 15:37:56 +0100

    Change network and add script to get files

---
+++ composer/getFiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo nc -l 1010 |tar xf -
+sudo nc -l 1010 |tar xf -
+sudo nc -l 1010 |tar xf -
--- setup2.sh
+++ setup2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -sSL https://goo.gl/6wtTN5 | bash -s 1.1.0
+export PATH=$PATH:~/fabric-samples/bin
