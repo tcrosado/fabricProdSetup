6771cb27e2d41775c91812e018bef88a95132ec2
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2018-09-19 15:06:24 +0100

    Add setup scripts

---
+++ setup1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
+chmod u+x prereqs-ubuntu.sh
+./prereqs-ubuntu.sh
+exit
+
---
+++ setup2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -sSL https://goo.gl/6wtTN5 | bash -s 1.1.0
